afdb89cf432c58ba9d5376189e9aeb2f71d1de99
nojf1 <160757983+nojf1@users.noreply.github.com>
2024-05-14 17:00:08 +0800

    created GameBoard with constants

---
+++ Main.java
@@ -0,0 +1,16 @@
+package battleship;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BattleShipsGameGUI game = new BattleShipsGameGUI();
+      
+
+    }
+    
+}
+
+/*
+ * 
+ */
