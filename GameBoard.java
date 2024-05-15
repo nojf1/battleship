a7151303d17d003b3ae917198932d5d0c3553632
nojf1 <160757983+nojf1@users.noreply.github.com>
2024-05-15 10:05:30 +0800

    added player ship placement and input validation

--- GameBoard.java
+++ GameBoard.java
@@ -1,5 +1,7 @@
 package battleship;
 
+import java.util.Scanner;
+
 public class GameBoard {
 
     public char[][] board; // Instance variable to store the board
@@ -9,8 +11,8 @@ public class GameBoard {
     //constructor for the GameBoard class
     public GameBoard() {
         this.board = new char[BOARD_SIZE][BOARD_SIZE]; // Initialize the board array
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            for (int j = 0; j < BOARD_SIZE; j++) {
+        for (int i = 0; i < BOARD_SIZE; i++) { //increment i when i is less than BOARD_SIZE
+            for (int j = 0; j < BOARD_SIZE; j++) { //increment j when j is less than BOARD_SIZE
                 this.board[i][j] = '~'; // Fill each cell with '~' so it looks like the sea
             }
         }
@@ -24,7 +26,7 @@ public class GameBoard {
         }
         System.out.println(); //prints new line to separate column headers from the board
 
-        for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int i = 0; i < BOARD_SIZE; i++) { //for loop to print the board until i is equal to BOARD_SIZE
             System.out.print(i + " "); // Row headers
             for (int j = 0; j < BOARD_SIZE; j++) { //increment j when j is less than BOARD_SIZE
                 System.out.print(board[i][j] + " "); //prints the board with spaces between each cell
@@ -33,5 +35,28 @@ public class GameBoard {
         }
 
     }
+
+    //method to place ships on the board
+    public void placeShips(char[][] board) {
+        Scanner scanner = new Scanner(System.in); //scanner object to get user input
+        System.out.println("Place your " + SHIPS + " ships:"); //prints the number of ships the player has to place
+
+        for (int i = 1; i <= SHIPS;) { //for loop to place ships until i is equal to SHIPS
+            System.out.print("Enter X coordinate for ship " + i + ": "); //prompts user to enter x coordinate
+            int x = scanner.nextInt(); //stores the x coordinate
+            System.out.print("Enter Y coordinate for ship " + i + ": "); //prompts user to enter y coordinate
+            int y = scanner.nextInt(); //stores the y coordinate
+
+            // check if coordinates are within bounds and the position is not already taken
+            if (x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE && board[x][y] == '~') {
+                board[x][y] = '@'; // place the player's ship
+                i++; // only increment if the ship is successfully placed
+            } else { // error message
+                System.out.println("Invalid coordinates or position already taken. Try again.");
+            }
+        }
+    }
+
+
 
 }
--- Main.java
+++ Main.java
@@ -2,9 +2,14 @@ package battleship;
 
 public class Main {
 
+    //main method to run program
     public static void main(String[] args) {
-        GameBoard playerBoard = new GameBoard();
-        playerBoard.printBoard(playerBoard.board);
+        GameBoard playerBoard = new GameBoard(); // Create a new GameBoard object for the player
+
+        playerBoard.printBoard(playerBoard.board);  // iniialise and print initial board
+        playerBoard.placeShips(playerBoard.board); // input for player to place ships
+
+        playerBoard.printBoard(playerBoard.board); // print the board with the ships placed
         
 
     }
@@ -12,9 +17,13 @@ public class Main {
 }
 
 /*
- * NOTES HERE
+ * NOTES/LOG HERE
  * 
  * The abundant amount of comments in the code is for personal understanding of
  * the code because
  * my brain cannot brain and will delete later for readability
+ * 
+ * 14/5/2024
+ * Finished the board for the player, will add the computer board next
+ * The board prints out as expected
  */
